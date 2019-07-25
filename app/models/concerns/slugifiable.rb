0d57ad2b133e9ced52c5986ae6c89c55c6e242e6
Jared Grove <jaredkgrove@gmail.com>
2019-07-25 16:12:04 +0000

    Automatically backed up by Learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,11 +1,11 @@
 module Slugifiable
   module ClassMethods
-    def slug
-      self.name.downcase.strip.gsub(" ","-")
-    end
+
   end
 
   module InstanceMethods
-
+    def slug
+      self.name.downcase.strip.gsub(" ","-")
+    end
   end
 end
